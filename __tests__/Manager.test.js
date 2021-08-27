9f611c2e99737414b5b8640ed2cc0a47029c6c6d
Electrolion <AMccollum71@gmail.com>
2021-08-27 15:54:24 -0400

    manager fail test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+const { TestWatcher } = require("jest");
+
+test("Set office number from constructor", () =>{
+    const testValue = 100; 
+    const e = new Manager("Adam", 1, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test('getRole() return "manager"', () => {
+    const testValue = "Manager";
+    const e = new Manager("Adam", 1, "test@test.com", 100);
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("get office number from getOfficeNumber()", () => {
+    const testValue = 80;
+    const e = new Manager("Adam", 1, "test@test.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+})
